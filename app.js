f64d5eeb9f72b6bb880399f21a01a3495bb736b5
Roman Sandarkin <sandarkin@gmail.com>
2023-11-24 00:00:57 +0100

    0205

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal: 'Become a Vue Master!',
             vueLink: 'https://vuejs.org'
         }
+    },
+    methods: {
+        outputGoal() {
+            const rnd = Math.random();
+            if (rnd < 0.5) {
+                return 'Learn Vue!';
+            } else {
+                return 'Master Vue!';
+            }
+        }
     }
 });
 app.mount('#goal');
