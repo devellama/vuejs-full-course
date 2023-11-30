1a20531f0bcdb2080d09e1e781dec43ff43d2d7e
Roman Sandarkin <sandarkin@gmail.com>
2023-11-30 11:13:03 +0100

    0208

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            name: 'Pasha',
-            age: 33,
-            imgUrl: 'https://images.unsplash.com/photo-1596083786324-86740304b605?w=240'
-        }
-    },
-    methods: {
-        calcAge() {
-            return this.age + 5;
-        },
-        calcRnd() {
-            return Math.random();
+            count: 0
         }
     }
 });
 
-app.mount('#exercise')
+app.mount('#events')
